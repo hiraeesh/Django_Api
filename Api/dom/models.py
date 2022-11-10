b5fe589047e17095c6d386251d41e5c226058608
Acetrot-Raeesh-Rayeen <raeesh.rayeen@acetrot.com>
2022-11-10 05:38:34 +0530

    add new files

--- Api/dom/models.py
+++ Api/dom/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class abc(models.Model):
+    fieldName = models.CharField(max_length = 150)
+    lname=models.CharField(max_length=40)
+    
